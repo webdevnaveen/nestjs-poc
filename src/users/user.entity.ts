63afd9524d238713bfc362a7692968b3c79dcd2b
webdevnaveen <ntnaveen241@gmail.com>
2025-01-06 13:21:18 +0530

    Add user module with entity for user management

---
+++ src/users/user.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column({ default: 'viewer' }) // Roles: admin, editor, viewer
+  role: string;
+}
